23f2bc4611d7ce5d9c55143d1fae9fdd4f443fc3
pulnip <choiwh1008@gmail.com>
2026-04-10 23:51:05 +0900

    refactor main logic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
+from datetime import date
 from dotenv import load_dotenv
 import os
 
@@ -7,52 +6,60 @@ import requests
 from io import BytesIO
 import pdfplumber
 
-KST = ZoneInfo("Asia/Seoul")
-today = datetime.now(KST).date()
-monday = today - timedelta(days=today.weekday())
-
 load_dotenv()
-PDF = os.environ["PDF_TEMPLATE"].replace('date', monday.strftime("%m%d"))
-WEB_URL = f"{os.environ["WEB_URL"]}{PDF}"
 WEBHOOK_URL = os.environ["WEBHOOK_URL"]
 
-def get_menu_table(url: str):
-    resp = requests.get(url)
+def make_menu_dict(table: list[list[str | None]]):
+    table = [row[2:] for row in table[2:-1]]
+    table[0][0] = "날짜"
+    table[-1][0] = "Take Out"
+
+    keys = {"날짜", "A코너", "B코너", "샐러드바", "잡곡밥", "후 식"}
+    key_index = {row[0]: i for i, row in enumerate(table) if row[0] in keys}
+
+    return{
+        "date"  : table[key_index["날짜"]][1:],
+        "a_red" : table[key_index["A코너"]][1:],
+        "a_menu": table[key_index["A코너"] + 1][1:],
+        "b_red" : table[key_index["B코너"]][1:],
+        "b_menu": table[key_index["B코너"] + 1][1:],
+        "salad" : table[key_index["샐러드바"]][1:],
+        "rice"  : table[key_index["잡곡밥"]][1:],
+        "drink" : table[key_index["후 식"]][1:],
+        "take_out": table[key_index["A코너"]][1:]
+    }
+
+def get_menu(url_template: str):
+    suffix = ['~', '_']
+    urls = [url_template.replace("{suffix}", s) for s in suffix]
+    for url in urls:
+        resp = requests.head(url=url)
+        if resp.status_code == 200:
+            resp = requests.get(url=url)
+
     print(resp)
     resp.raise_for_status()
 
     with pdfplumber.open(BytesIO(resp.content)) as pdf:
         table = pdf.pages[0].extract_table()
 
-    return table
+    return make_menu_dict(table)
 
-def get_today_menu(table: list[list[str | None]]):
-    header = table[2]
-    col = next(i for i, cell in enumerate(header) if cell and today.isoformat() in cell)
-
-    items: list[str] = []
-    for row in table[3:]:  # 날짜 행 이후부터
-        if col < len(row) and row[col] and row[col].strip():
-            items.append(row[col].strip())
+def get_today_menu(menu: dict[str, list[str]], today: date):
+    dates = menu['date']
+    col = next(i for i, cell in enumerate(dates) if cell and today.isoformat() in cell)
 
     return{
-        "a_red" : items[0].split('\n'),
-        "a_menu": items[1].split('\n'),
-        "b_red" : items[2].split('\n'),
-        "b_menu": items[3].split('\n'),
-        "salad" : items[4].split('\n') + items[5].split('\n'),
-        "rice"  : items[6].split('\n'),
-        "drink" : items[7].split('\n'),
-        "take_out": items[8].split('\n')
+        key: value[col] for key, value in menu.items()
     }
 
 def to_markdown_str(menu: dict[str: list[str]]):
     return (
         "## 오늘의 점심 메뉴를 알려드립니다.\n"
         "### A코스\n"
-        f"{"\n".join(menu['a_red'] + menu['a_menu'])}\n"
+        f"{menu['a_red']}\n{menu['a_menu']}\n"
         "### B코스\n"
-        f"{"\n".join(menu['b_red'] + menu['b_menu'])}\n"
+        f"{menu['b_red']}\n{menu['b_menu']}\n"
     )
 
 def post_to_chat(url: str, content: str):
@@ -63,11 +70,24 @@ def post_to_chat(url: str, content: str):
         }
     )
 
-def run():
+def run(today: date=None):
+    if today is None:
+        from zoneinfo import ZoneInfo
+        from datetime import datetime
+
+        KST = ZoneInfo("Asia/Seoul")
+        today = datetime.now(KST).date()
+
+    from datetime import timedelta
+
+    monday = today - timedelta(days=today.weekday())
+    PDF_NAME = os.environ["PDF_NAME_TEMPLATE"].replace("{date}", monday.strftime("%m%d"))
+    WEB_URL_TEMPLATE = f"{os.environ["WEB_URL_PREFIX"]}{PDF_NAME}"
+
     try:
-        table = get_menu_table(url=WEB_URL)
-        menu = get_today_menu(table=table)
-        md = to_markdown_str(menu=menu)
+        weekly_menu = get_menu(url_template=WEB_URL_TEMPLATE)
+        today_menu = get_today_menu(menu=weekly_menu, today=today)
+        md = to_markdown_str(menu=today_menu)
     except requests.HTTPError:
         md = "점심 메뉴를 찾을 수 없습니다. ㅠㅠ"
     except Exception:
